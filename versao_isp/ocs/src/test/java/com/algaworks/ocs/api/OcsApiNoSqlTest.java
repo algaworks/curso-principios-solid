ad62f71d48255834f1af3da51902cbd31add1ca9
Normandes Junior <normandesjr@gmail.com>
2015-02-25 08:44:29 -0300

    Versao ISP

--- versao_isp/ocs/src/test/java/com/algaworks/ocs/api/OcsApiNoSqlTest.java
+++ versao_isp/ocs/src/test/java/com/algaworks/ocs/api/OcsApiNoSqlTest.java
@@ -3,23 +3,18 @@ package com.algaworks.ocs.api;
 import com.algaworks.ocs.cdr.CDRFileLocator;
 import com.algaworks.ocs.cdr.CDRGenerator;
 import com.algaworks.ocs.cdr.CDRGeneratorFile;
+import com.algaworks.ocs.cdr.CDRGeneratorNoSql;
 
 public class OcsApiNoSqlTest extends OcsApi {
 
-	private CDRGeneratorFile cdrGeneratorFile;
-	
-	public OcsApiNoSqlTest() {
-		this.cdrGeneratorFile = new CDRGeneratorFile("/opt/cdr");
-	}
-
 	@Override
 	public CDRGenerator getCdrGenerator() {
-		return cdrGeneratorFile;
+		return new CDRGeneratorNoSql();
 	}
 
 	@Override
 	public CDRFileLocator getCdrFileLocator() {
-		return cdrGeneratorFile;
+		return null;
 	}
 
 }
